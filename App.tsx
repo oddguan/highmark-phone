d90254af6584234dda77b2588340cf411632cd71
Chenxiao Guan <1011zaozao@gmail.com>
2020-10-08 11:52:18 -0400

    theming

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
+
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +26,12 @@ export default function App() {
     return null;
   } else {
     return (
+      <PaperProvider theme={theme}>
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
+      </PaperProvider>
     );
   }
 }
